d6d2a092ed36e9f31cd14a32e389fc4901bdabf1
Vadym <lidiya.park@gmail.com>
2021-02-11 16:03:06 +0200

    test1

--- src/main/java/com/tinder/connection/DbConnection.java
+++ src/main/java/com/tinder/connection/DbConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DbConnection {
 
-    private String path = "jdbc:postgresql://aizffrzhrbusqj:ec895b98cf773ca62ae900e1a49510de5b1b62d3e38ea58d5082c08c576bf92f@ec2-18-204-74-74.compute-1.amazonaws.com:5432/d65gpbfgs75bhf";
-    private String username = "aizffrzhrbusqj";
-    private String password = "ec895b98cf773ca62ae900e1a49510de5b1b62d3e38ea58d5082c08c576bf92f";
+    private String path = "postgres://dldplmkdamdbnf:4d5090f9d6bd25656771cf8af9d5cb1dbf33d69934e58f3bda85a5e7a32b4cbc@ec2-52-7-168-69.compute-1.amazonaws.com:5432/d113dena0hmoph";
+    private String username = "dldplmkdamdbnf";
+    private String password = "4d5090f9d6bd25656771cf8af9d5cb1dbf33d69934e58f3bda85a5e7a32b4cbc";
 
     private Connection connection = null;
 
